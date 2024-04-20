c9aada223180470ba5c5f7746189ea32aa97ca24
Kosta <Kostaricanec228@yandex.ru>
2024-04-20 20:18:10 +0400

    homework

--- src/lesson17/Main2.java
+++ src/lesson17/Main2.java
@@ -1,5 +1,7 @@
 package lesson17;
 
+import java.util.Arrays;
+
 public class Main2 {
     public static void main(String[] args) {
         StringUtilsImpl stringUtilsimpl = new StringUtilsImpl();
@@ -7,14 +9,49 @@ public class Main2 {
         String num2 = "0";
 
         try {
-            double result = stringUtilsimpl.div(num1,num2);
-            System.out.println("результат: "+result);
-        }catch (NullPointerException e){
+            double result = stringUtilsimpl.div(num1, num2);
+            System.out.println("результат: " + result);
+        } catch (NullPointerException e) {
             System.out.println(e.getMessage());
-        }catch (NumberFormatException e){
+        } catch (NumberFormatException e) {
             System.out.println(e.getMessage());
-        }catch (ArithmeticException e){
+        } catch (ArithmeticException e) {
             System.out.println(e.getMessage());
         }
+//
+        StringUtilsImpl wordFinder = new StringUtilsImpl();
+        String text = "У лукоморья дуб зеленый";
+        String word = "дб";
+
+        try {
+            int[] indices = wordFinder.findWord(text, word);
+
+            if (indices.length == 0) {
+                System.out.println("слово не найдено");
+            } else {
+                System.out.println("слово найдено по индексу: ");
+                for (int index : indices) {
+                    System.out.print(index + " ");
+                }
+            }
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerException caught: " + e.getMessage());
+        }
+//
+        StringUtilsImpl numbFinder = new StringUtilsImpl();
+        String text = "у лукоморья 12345 дуба";
+
+        try {
+            double[] numbers = numbFinder.findNumbers(text);
+
+            for (double number : numbers) {
+                System.out.println(number);
+            }
+        } catch (CustomException e) {
+            System.out.println("Error: " + e.getMessage());
+
+        }
+
     }
 }
+
--- src/lesson17/StringUtilsImpl.java
+++ src/lesson17/StringUtilsImpl.java
@@ -1,5 +1,10 @@
 package lesson17;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringUtilsImpl implements StringUtils {
     @Override
     public double div(String number1, String number2) throws NullPointerException, NumberFormatException, ArithmeticException {
@@ -10,7 +15,7 @@ public class StringUtilsImpl implements StringUtils {
         int num1 = Integer.parseInt(number1);
         int num2 = Integer.parseInt(number2);
 
-        if (number2 == null) {
+        if (num2 == 0) {
             throw new ArithmeticException("на ноль делить нельзя");
         }
         return num1 / num2;
@@ -18,11 +23,42 @@ public class StringUtilsImpl implements StringUtils {
 
     @Override
     public int[] findWord(String text, String word) throws NullPointerException {
-        return new int[0];
+        if (text == null || word == null) {
+            throw new NullPointerException("text == null || word == null");
+        }
+        List<Integer> indices = new ArrayList<>();
+        int index = text.indexOf(word);
+
+        while (index >= 0) {
+            indices.add(index);
+            index = text.indexOf(word, index + 1);
+        }
+        return indices.stream().mapToInt(Integer::intValue).toArray();
     }
 
     @Override
     public double[] findNumbers(String text) throws CustomException {
-        return new double[0];
+
+        if (text == null){
+            throw new CustomException("text == null");
+        }
+
+        List<Double> numbers = new ArrayList<>();
+        Pattern pattern = Pattern.compile("\\d+(\\.\\d+)?");
+        Matcher matcher = pattern.matcher(text);
+
+        while (matcher.find()){
+            try {
+                numbers.add(Double.parseDouble(matcher.group()));
+            }catch (NumberFormatException e){
+                throw new CustomException("Error parsing number: "+ e.getMessage());
+            }
+        }
+        if (numbers.isEmpty()){
+            throw new CustomException("not found");
+        }
+        return numbers.stream().mapToDouble(Double::doubleValue).toArray();
+
+
     }
 }
